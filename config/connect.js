b340e0d36feb7c63b1438086a335089f0235bbf7
swapankumarsardar <swapankumarsardar73727@gmail.com>
2022-06-07 18:25:05 +0530

    fixed pass

--- config/connect.js
+++ config/connect.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 const config = require('./config')
 
-const connection = mongoose.connect(config.MONGO_URL,{ useNewUrlParser: true }).then(res=>{
-    console.log(`successfully connected to ${config.MONGO_URL}`);
+const connection = mongoose.connect(config.ATLAS_URL,{ useNewUrlParser: true }).then(res=>{
+    console.log(`successfully connected to ${config.ATLAS_URL}`);
 }).catch(err => {
     console.log(err);
-    console.log(`cannot connect to the the ${config.MONGO_URL} `)
+    console.log(`cannot connect to the the ${config.ATLAS_URL} `)
 })
 
 module.exports = connection
